0e31eb16610d8993b5de6042a477512e818d2804
Weisong Zheng <z00647347@gmail.com>
2026-09-14 20:25:22 +0800

    refactor: Maven多模块化

--- tlias-web-managment/src/main/java/org/example/interceptor/TokenInterceptor.java
+++ tlias-web-managment/src/main/java/org/example/interceptor/TokenInterceptor.java
@@ -5,10 +5,8 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.example.utils.JwtUtils;
-import org.jspecify.annotations.Nullable;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
-import org.springframework.web.servlet.ModelAndView;
 
 @Slf4j
 @Component
